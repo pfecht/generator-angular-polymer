4ecaa21025456b9563c0c404b07ccbf0a22248c4
Pascal Fecht <pfecht@inovex.de>
2017-04-07 14:29:24 +0200

    Changing tests

--- test/main-document.js
+++ test/main-document.js
@@ -1,15 +1,1 @@
-'use strict';
-const path = require('path');
-const helpers = require('yeoman-test');
-const assert = require('yeoman-assert');
-const generatorsDir = "../generators";
-
-describe('angular-polymer:init', () => {
-  beforeEach(function (done) {
-    helpers.run(require.resolve(path.join(generatorsDir, '/main-document'))).on('end', done);
-  });
-
-  it('generates the default configuration', function () {
-    //assert.file('.yo-rc.json');
-  });
-});
+// TODO
